aa6eda1e2f716ca7685c723bc2fd482c2c33c63d
kdm <cokj1610@naver.com>
2020-08-28 12:24:38 +0900

    테스트 갤럭시20크롤링

---
+++ src/test/java/com/cos/review/ChefBackRecipy.java
@@ -0,0 +1,26 @@
+package com.cos.review;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+//private int id;
+//private String blogUrl;
+//private String title;
+//private String thumnail;
+//private String day;
+
+import com.cos.review.model.Product;
+public class ChefBackRecipy {
+
+	@Test
+	public void 백종원레시피_블로그_크롱링() {
+		int start= 1;
+		List<Product> products = new ArrayList<>();
+		while(products.size() <1001) {
+			String url = "";
+		}
+	}
+	
+	
+}
